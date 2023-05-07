c28447a08fafb62431142763c4553a946a133862
Gustavo <gustavoortigoza06@gmail.com>
2023-05-07 09:18:55 -0400

    Se ha añadido la ruta de pacientes al middleware

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import {PORT} from './config.js';
+import pacientesRoute from './routes/pacienteRoute.js'
 
 const app = express();
 
 //procesa los datos del cliente en formato json
 app.use(express.json());
+
+app.use(pacientesRoute);
 
 app.listen(PORT);
 console.log(`El servidor se esta ejecutando en el puerto ${PORT}`);
