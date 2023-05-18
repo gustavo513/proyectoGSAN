8ce3cfeb4f7c2ded0df71e090de0206858185351
Gustavo <gustavoortigoza06@gmail.com>
2023-05-17 21:45:23 -0400

    Se han añadido las tablas medicos y horariosmedicos a la base de datos

--- script_bd/gsan.sql
+++ script_bd/gsan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-05-2023 a las 04:13:36
+-- Tiempo de generación: 18-05-2023 a las 03:38:19
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.1.17
 
@@ -63,6 +63,96 @@ INSERT INTO `ciudades` (`ciudadId`, `descripcion`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `especialidades`
+--
+
+CREATE TABLE `especialidades` (
+  `especialidadId` int(11) NOT NULL,
+  `descripcion` varchar(35) NOT NULL,
+  `estado` varchar(1) DEFAULT 'D'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `especialidades`
+--
+
+INSERT INTO `especialidades` (`especialidadId`, `descripcion`, `estado`) VALUES
+(1, 'Neurologia', 'D'),
+(2, 'Cirugia', 'D');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `horarios`
+--
+
+CREATE TABLE `horarios` (
+  `horarioId` int(11) NOT NULL,
+  `dia` int(1) NOT NULL,
+  `desde` time NOT NULL,
+  `hasta` time NOT NULL,
+  `estado` varchar(1) DEFAULT 'D'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `horarios`
+--
+
+INSERT INTO `horarios` (`horarioId`, `dia`, `desde`, `hasta`, `estado`) VALUES
+(1, 2, '07:00:00', '15:00:00', 'D'),
+(2, 2, '15:00:00', '23:00:00', 'D');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `horariosmedicos`
+--
+
+CREATE TABLE `horariosmedicos` (
+  `horarioMedicoId` int(11) NOT NULL,
+  `medicoId` int(11) NOT NULL,
+  `horarioId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `horariosmedicos`
+--
+
+INSERT INTO `horariosmedicos` (`horarioMedicoId`, `medicoId`, `horarioId`) VALUES
+(1, 1, 1),
+(2, 7, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `medicos`
+--
+
+CREATE TABLE `medicos` (
+  `medicoId` int(11) NOT NULL,
+  `nombre` varchar(35) NOT NULL,
+  `apellido` varchar(35) NOT NULL,
+  `ci` varchar(10) NOT NULL,
+  `fechaNac` date NOT NULL,
+  `telefono` varchar(10) NOT NULL,
+  `direccion` varchar(50) DEFAULT NULL,
+  `intervConsulta` int(11) DEFAULT NULL,
+  `estado` varchar(1) DEFAULT 'D',
+  `especialidadId` int(11) NOT NULL,
+  `usuarioId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `medicos`
+--
+
+INSERT INTO `medicos` (`medicoId`, `nombre`, `apellido`, `ci`, `fechaNac`, `telefono`, `direccion`, `intervConsulta`, `estado`, `especialidadId`, `usuarioId`) VALUES
+(1, 'Jorge', 'Rojas', '4531532', '1989-08-26', '0983125820', 'Gral. Bernardino Caballero - Pdte. Franco', 15, 'D', 1, 1),
+(7, 'Victor', 'Castro', '4343123', '1987-07-12', '0983152312', 'Av. Heroes del Chaco', 15, 'O', 2, 1);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `pacientes`
 --
 
@@ -99,22 +189,23 @@ INSERT INTO `pacientes` (`pacienteId`, `nombre`, `apellido`, `ci`, `fechaNac`, `
 --
 
 CREATE TABLE `turnos` (
-  `id` int(11) NOT NULL,
-  `fecha` timestamp NULL DEFAULT current_timestamp(),
+  `turnoId` int(11) NOT NULL,
+  `fecha` date NOT NULL,
   `hora` int(11) NOT NULL,
   `estado` varchar(1) DEFAULT 'P',
   `total` int(11) NOT NULL,
-  `pacienteid` int(11) NOT NULL,
-  `medicoid` int(11) NOT NULL,
-  `usuarioid` int(11) NOT NULL
+  `fechaReg` timestamp NULL DEFAULT current_timestamp(),
+  `pacienteId` int(11) NOT NULL,
+  `medicoId` int(11) NOT NULL,
+  `usuarioId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Volcado de datos para la tabla `turnos`
 --
 
-INSERT INTO `turnos` (`id`, `fecha`, `hora`, `estado`, `total`, `pacienteid`, `medicoid`, `usuarioid`) VALUES
-(4, '2023-05-12 01:33:24', 4, 'P', 60000, 1, 1, 1);
+INSERT INTO `turnos` (`turnoId`, `fecha`, `hora`, `estado`, `total`, `fechaReg`, `pacienteId`, `medicoId`, `usuarioId`) VALUES
+(4, '2023-05-11', 4, 'P', 60000, '2023-05-13 13:45:16', 1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -154,6 +245,34 @@ ALTER TABLE `ciudades`
   ADD PRIMARY KEY (`ciudadId`);
 
 --
+-- Indices de la tabla `especialidades`
+--
+ALTER TABLE `especialidades`
+  ADD PRIMARY KEY (`especialidadId`);
+
+--
+-- Indices de la tabla `horarios`
+--
+ALTER TABLE `horarios`
+  ADD PRIMARY KEY (`horarioId`);
+
+--
+-- Indices de la tabla `horariosmedicos`
+--
+ALTER TABLE `horariosmedicos`
+  ADD PRIMARY KEY (`horarioMedicoId`),
+  ADD KEY `medicoId` (`medicoId`),
+  ADD KEY `horarioId` (`horarioId`);
+
+--
+-- Indices de la tabla `medicos`
+--
+ALTER TABLE `medicos`
+  ADD PRIMARY KEY (`medicoId`),
+  ADD KEY `usuarioId` (`usuarioId`),
+  ADD KEY `especialidadId` (`especialidadId`);
+
+--
 -- Indices de la tabla `pacientes`
 --
 ALTER TABLE `pacientes`
@@ -167,7 +286,10 @@ ALTER TABLE `pacientes`
 -- Indices de la tabla `turnos`
 --
 ALTER TABLE `turnos`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`turnoId`),
+  ADD KEY `pacienteid` (`pacienteId`),
+  ADD KEY `usuarioId` (`usuarioId`),
+  ADD KEY `medicoId` (`medicoId`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -193,6 +315,30 @@ ALTER TABLE `ciudades`
   MODIFY `ciudadId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT de la tabla `especialidades`
+--
+ALTER TABLE `especialidades`
+  MODIFY `especialidadId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `horarios`
+--
+ALTER TABLE `horarios`
+  MODIFY `horarioId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `horariosmedicos`
+--
+ALTER TABLE `horariosmedicos`
+  MODIFY `horarioMedicoId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `medicos`
+--
+ALTER TABLE `medicos`
+  MODIFY `medicoId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
 -- AUTO_INCREMENT de la tabla `pacientes`
 --
 ALTER TABLE `pacientes`
@@ -202,7 +348,7 @@ ALTER TABLE `pacientes`
 -- AUTO_INCREMENT de la tabla `turnos`
 --
 ALTER TABLE `turnos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `turnoId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
@@ -215,12 +361,34 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- Filtros para la tabla `horariosmedicos`
+--
+ALTER TABLE `horariosmedicos`
+  ADD CONSTRAINT `horariosmedicos_ibfk_1` FOREIGN KEY (`medicoId`) REFERENCES `medicos` (`medicoId`),
+  ADD CONSTRAINT `horariosmedicos_ibfk_2` FOREIGN KEY (`horarioId`) REFERENCES `horarios` (`horarioid`);
+
+--
+-- Filtros para la tabla `medicos`
+--
+ALTER TABLE `medicos`
+  ADD CONSTRAINT `medicos_ibfk_1` FOREIGN KEY (`usuarioId`) REFERENCES `usuarios` (`usuarioId`),
+  ADD CONSTRAINT `medicos_ibfk_2` FOREIGN KEY (`especialidadId`) REFERENCES `especialidades` (`especialidadid`);
+
+--
 -- Filtros para la tabla `pacientes`
 --
 ALTER TABLE `pacientes`
   ADD CONSTRAINT `pacientes_ibfk_1` FOREIGN KEY (`usuarioId`) REFERENCES `usuarios` (`usuarioId`),
   ADD CONSTRAINT `pacientes_ibfk_2` FOREIGN KEY (`barrioId`) REFERENCES `barrios` (`barrioId`),
   ADD CONSTRAINT `pacientes_ibfk_3` FOREIGN KEY (`ciudadId`) REFERENCES `ciudades` (`ciudadId`);
+
+--
+-- Filtros para la tabla `turnos`
+--
+ALTER TABLE `turnos`
+  ADD CONSTRAINT `turnos_ibfk_1` FOREIGN KEY (`pacienteId`) REFERENCES `pacientes` (`pacienteId`),
+  ADD CONSTRAINT `turnos_ibfk_2` FOREIGN KEY (`usuarioId`) REFERENCES `usuarios` (`usuarioId`),
+  ADD CONSTRAINT `turnos_ibfk_3` FOREIGN KEY (`medicoId`) REFERENCES `medicos` (`medicoId`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
