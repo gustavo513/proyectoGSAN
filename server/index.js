50a93e18c1bff2a0642eac1f7ec8f82c32851c93
eduardo <cuvaseduardo0623@gmail.com>
2023-05-18 20:10:38 -0400

    Se han añadido los controladores y rutas de los campos especialidades y horarios, juntos con cambios en el index.js

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ import ciudadesRoute from './routes/ciudadRoute.js';
 import barriosRoute from './routes/barrioRoute.js';
 import usuariosRoute from './routes/usuarioRoute.js';
 import turnoRoute from './routes/turnoRoute.js';
+import horariosRoute from './routes/horarioRoute.js';
+import especialidadesRoute from './routes/especialidadRoute.js';
 
 const app = express();
 
@@ -16,6 +18,8 @@ app.use(ciudadesRoute);
 app.use(barriosRoute);
 app.use(usuariosRoute);
 app.use(turnoRoute);
+app.use(horariosRoute);
+app.use(especialidadesRoute);
 
 app.listen(PORT);
 console.log(`El servidor se esta ejecutando en el puerto ${PORT}`);
