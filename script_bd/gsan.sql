1f411d5ecab01699e49bae5af957a9b1cc6183ec
Gustavo <gustavoortigoza06@gmail.com>
2023-06-30 21:16:18 -0400

    Se ha agregado el Trigger actualizar_ultTurno_pacientes a la base de datos

--- script_bd/gsan.sql
+++ script_bd/gsan.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-05-2023 a las 03:38:19
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.1.17
+-- Tiempo de generación: 01-07-2023 a las 03:13:36
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.0.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `barrios` (
   `barrioId` int(11) NOT NULL,
   `descripcion` varchar(35) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `barrios`
@@ -39,7 +39,8 @@ CREATE TABLE `barrios` (
 INSERT INTO `barrios` (`barrioId`, `descripcion`) VALUES
 (1, 'San Juan'),
 (2, 'Ciudad Nueva'),
-(3, 'Area 1');
+(3, 'Area 1'),
+(4, 'Santa Ana');
 
 -- --------------------------------------------------------
 
@@ -50,7 +51,7 @@ INSERT INTO `barrios` (`barrioId`, `descripcion`) VALUES
 CREATE TABLE `ciudades` (
   `ciudadId` int(11) NOT NULL,
   `descripcion` varchar(35) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `ciudades`
@@ -58,7 +59,9 @@ CREATE TABLE `ciudades` (
 
 INSERT INTO `ciudades` (`ciudadId`, `descripcion`) VALUES
 (1, 'Ciudad del Este'),
-(2, 'Pdte. Franco');
+(2, 'Pdte. Franco'),
+(3, 'Minga Guazu'),
+(4, 'Juan E. O\'Leary');
 
 -- --------------------------------------------------------
 
@@ -70,7 +73,7 @@ CREATE TABLE `especialidades` (
   `especialidadId` int(11) NOT NULL,
   `descripcion` varchar(35) NOT NULL,
   `estado` varchar(1) DEFAULT 'D'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `especialidades`
@@ -78,7 +81,11 @@ CREATE TABLE `especialidades` (
 
 INSERT INTO `especialidades` (`especialidadId`, `descripcion`, `estado`) VALUES
 (1, 'Neurologia', 'D'),
-(2, 'Cirugia', 'D');
+(2, 'Cirugia', 'I'),
+(3, 'Pediatria', 'D'),
+(4, 'Psicologia', 'D'),
+(5, 'Traumatología', 'D'),
+(6, 'Dermatología', 'D');
 
 -- --------------------------------------------------------
 
@@ -92,15 +99,20 @@ CREATE TABLE `horarios` (
   `desde` time NOT NULL,
   `hasta` time NOT NULL,
   `estado` varchar(1) DEFAULT 'D'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `horarios`
 --
 
 INSERT INTO `horarios` (`horarioId`, `dia`, `desde`, `hasta`, `estado`) VALUES
-(1, 2, '07:00:00', '15:00:00', 'D'),
-(2, 2, '15:00:00', '23:00:00', 'D');
+(1, 3, '15:00:00', '23:00:00', 'D'),
+(2, 2, '15:00:00', '23:00:00', 'D'),
+(3, 2, '07:00:00', '15:00:00', 'D'),
+(4, 6, '07:00:00', '15:00:00', 'I'),
+(5, 4, '07:00:00', '15:00:00', 'D'),
+(6, 5, '07:00:00', '15:00:00', 'D'),
+(7, 7, '07:00:00', '15:00:00', 'I');
 
 -- --------------------------------------------------------
 
@@ -112,7 +124,7 @@ CREATE TABLE `horariosmedicos` (
   `horarioMedicoId` int(11) NOT NULL,
   `medicoId` int(11) NOT NULL,
   `horarioId` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `horariosmedicos`
@@ -120,7 +132,8 @@ CREATE TABLE `horariosmedicos` (
 
 INSERT INTO `horariosmedicos` (`horarioMedicoId`, `medicoId`, `horarioId`) VALUES
 (1, 1, 1),
-(2, 7, 1);
+(2, 7, 1),
+(6, 11, 2);
 
 -- --------------------------------------------------------
 
@@ -140,15 +153,17 @@ CREATE TABLE `medicos` (
   `estado` varchar(1) DEFAULT 'D',
   `especialidadId` int(11) NOT NULL,
   `usuarioId` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `medicos`
 --
 
 INSERT INTO `medicos` (`medicoId`, `nombre`, `apellido`, `ci`, `fechaNac`, `telefono`, `direccion`, `intervConsulta`, `estado`, `especialidadId`, `usuarioId`) VALUES
-(1, 'Jorge', 'Rojas', '4531532', '1989-08-26', '0983125820', 'Gral. Bernardino Caballero - Pdte. Franco', 15, 'D', 1, 1),
-(7, 'Victor', 'Castro', '4343123', '1987-07-12', '0983152312', 'Av. Heroes del Chaco', 15, 'O', 2, 1);
+(1, 'Jorge', 'Rojas', '4531532', '1989-08-26', '0983125820', 'Gral. Bernardino Caballero - Pdte. Franco', 15, 'D', 2, 1),
+(7, 'Victor', 'Castro', '4343123', '1987-07-12', '0983152312', 'Av. Heroes del Chaco', 15, 'D', 2, 1),
+(11, 'Gustavo', 'Ortigoza Andino', '5300653', '2001-10-14', '0983115720', 'Barrio San Juan km 8 Acararay - Ciudad del Este', 15, 'A', 1, 1),
+(12, 'Gustavo', 'Andino', '5300654', '2001-10-14', '0983115720', 'Barrio San Juan km 8 Acararay - Ciudad del Este', 15, 'D', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -171,16 +186,17 @@ CREATE TABLE `pacientes` (
   `fechaReg` timestamp NULL DEFAULT current_timestamp(),
   `ultTurno` timestamp NULL DEFAULT NULL,
   `usuarioId` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `pacientes`
 --
 
 INSERT INTO `pacientes` (`pacienteId`, `nombre`, `apellido`, `ci`, `fechaNac`, `sexo`, `sangre`, `barrioId`, `ciudadId`, `direccion`, `telefono`, `fechaReg`, `ultTurno`, `usuarioId`) VALUES
-(1, 'Juan', 'Perez', '5300653', '2001-10-14', 'M', 'RH', 1, 1, 'Calle Univesidad Nacional del Este - km 8 Acaray', '0983115720', '2023-05-07 04:00:00', NULL, 1),
+(1, 'Juan', 'Perez', '5300653', '2001-10-14', 'M', 'RH', 1, 1, 'Calle Univesidad Nacional del Este - km 8 Acaray', '0983115720', '2023-05-07 04:00:00', '2023-07-01 01:12:22', 1),
 (2, 'Maria', 'Ojeda', '5532532', '2003-06-07', 'F', 'A', 2, 1, 'Avenida Adrian Jara - Calle Boqueron', '0981532341', '2023-05-07 04:00:00', NULL, 1),
-(6, 'Benjamin', 'Abraham', '5312432', '2023-01-01', 'M', 'A', 1, 1, 'Calle Av. Peru', '0983115252', '2023-05-11 04:00:00', NULL, 1);
+(6, 'Benjamin', 'Abraham', '5312432', '2023-01-01', 'M', 'A', 1, 1, 'Calle Av. Peru', '0983115252', '2023-05-11 04:00:00', '2023-06-28 21:09:34', 1),
+(10, 'Gustavo', 'Ortigoza', '5304312', '2001-10-12', 'M', 'RH', 2, 1, 'Av. Proceres de Mayo', '534', '2023-06-07 17:31:34', NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -198,14 +214,24 @@ CREATE TABLE `turnos` (
   `pacienteId` int(11) NOT NULL,
   `medicoId` int(11) NOT NULL,
   `usuarioId` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `turnos`
 --
 
 INSERT INTO `turnos` (`turnoId`, `fecha`, `hora`, `estado`, `total`, `fechaReg`, `pacienteId`, `medicoId`, `usuarioId`) VALUES
-(4, '2023-05-11', 4, 'P', 60000, '2023-05-13 13:45:16', 1, 1, 1);
+(4, '2023-05-11', 4, 'C', 60000, '2023-05-13 13:45:16', 1, 1, 1),
+(5, '2023-06-30', 2, 'F', 60000, '2023-06-28 21:08:59', 6, 1, 1),
+(6, '2023-07-21', 2, 'P', 60000, '2023-07-01 01:12:22', 1, 7, 1);
+
+--
+-- Disparadores `turnos`
+--
+DELIMITER $$
+CREATE TRIGGER `actualizar_ultTurno_pacientes` AFTER INSERT ON `turnos` FOR EACH ROW UPDATE pacientes SET ultTurno = current_timestamp() WHERE pacienteId = NEW.pacienteId
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -218,7 +244,7 @@ CREATE TABLE `usuarios` (
   `usuario` varchar(30) DEFAULT NULL,
   `codigo` int(11) DEFAULT NULL,
   `permisos` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Volcado de datos para la tabla `usuarios`
@@ -306,49 +332,49 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `barrios`
 --
 ALTER TABLE `barrios`
-  MODIFY `barrioId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `barrioId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `ciudades`
 --
 ALTER TABLE `ciudades`
-  MODIFY `ciudadId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ciudadId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `especialidades`
 --
 ALTER TABLE `especialidades`
-  MODIFY `especialidadId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `especialidadId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `horarios`
 --
 ALTER TABLE `horarios`
-  MODIFY `horarioId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `horarioId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `horariosmedicos`
 --
 ALTER TABLE `horariosmedicos`
-  MODIFY `horarioMedicoId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `horarioMedicoId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `medicos`
 --
 ALTER TABLE `medicos`
-  MODIFY `medicoId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `medicoId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `pacientes`
 --
 ALTER TABLE `pacientes`
-  MODIFY `pacienteId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `pacienteId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de la tabla `turnos`
 --
 ALTER TABLE `turnos`
-  MODIFY `turnoId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `turnoId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
@@ -365,14 +391,14 @@ ALTER TABLE `usuarios`
 --
 ALTER TABLE `horariosmedicos`
   ADD CONSTRAINT `horariosmedicos_ibfk_1` FOREIGN KEY (`medicoId`) REFERENCES `medicos` (`medicoId`),
-  ADD CONSTRAINT `horariosmedicos_ibfk_2` FOREIGN KEY (`horarioId`) REFERENCES `horarios` (`horarioid`);
+  ADD CONSTRAINT `horariosmedicos_ibfk_2` FOREIGN KEY (`horarioId`) REFERENCES `horarios` (`horarioId`);
 
 --
 -- Filtros para la tabla `medicos`
 --
 ALTER TABLE `medicos`
   ADD CONSTRAINT `medicos_ibfk_1` FOREIGN KEY (`usuarioId`) REFERENCES `usuarios` (`usuarioId`),
-  ADD CONSTRAINT `medicos_ibfk_2` FOREIGN KEY (`especialidadId`) REFERENCES `especialidades` (`especialidadid`);
+  ADD CONSTRAINT `medicos_ibfk_2` FOREIGN KEY (`especialidadId`) REFERENCES `especialidades` (`especialidadId`);
 
 --
 -- Filtros para la tabla `pacientes`
